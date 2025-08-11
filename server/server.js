8f0765e987be11bd74da39b4a9e26e6ed0404aaf
Mohit <mohitthakur.stark@gmail.com>
2025-08-11 13:12:19 +0530

    initial updates

--- server/server.js
+++ server/server.js
@@ -11,53 +11,50 @@ import authRoutes from "./routes/authRoutes.js";
 import projectRoutes from "./routes/projectRoutes.js";
 
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
+
 
 app.use(cors({
   origin: process.env.FRONTEND_URL,
   credentials: true,
 }));
-
 app.use(express.json());
 
 
 app.use("/auth", authRoutes);
 app.use("/projects", projectRoutes);
 
+
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  const statusCode = err.statusCode || 500;
-  res.status(statusCode).json({
-    error: err.message || "Internal Server Error",
-  });
+  res.status(err.statusCode || 500).json({ error: err.message || "Internal Server Error" });
 });
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 
-const frontendPath = path.join(__dirname, "client", "dist");
-console.log("Frontend path:", frontendPath);
-console.log("Dist exists:", fs.existsSync(frontendPath));
+if (process.env.NODE_ENV === "production") {
+  const frontendPath = path.join(__dirname, "..", "client", "dist");
+  console.log("Frontend path:", frontendPath);
+  console.log("Dist exists:", fs.existsSync(frontendPath));
 
-if (fs.existsSync(frontendPath)) {
-  app.use(express.static(frontendPath));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(frontendPath, "index.html"));
-  });
-} else {
-  console.warn("⚠️ Dist folder not found — frontend will not be served.");
+  if (fs.existsSync(frontendPath)) {
+    app.use(express.static(frontendPath));
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(frontendPath, "index.html"));
+      console.log("dist file")
+    });
+  } else {
+    console.warn("⚠️ Dist folder not found — frontend will not be served.");
+  }
 }
 
 const startServer = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to MongoDB");
-
-    app.listen(port, () => {
-      console.log(`Server running on ${process.env.BACKEND_URL || `http://localhost:${port}`}`);
-    });
+    app.listen(port, () => console.log(`Server running on port ${port}`));
   } catch (error) {
     console.error("DB Connection Failed:", error.message);
     process.exit(1);
